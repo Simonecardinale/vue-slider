7308401682df644c524d2706e6b621361b04c6a1
Simone Cardinale <simonecardinale8@gmail.com>
2021-01-26 14:51:07 +0100

    next and prev complete

---
+++ js/script.js
@@ -0,0 +1,28 @@
+var app = new Vue ({
+    el: '#container',
+    data: {
+        imgArray: ["https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg", 
+        "https://cdn.pixabay.com/photo/2014/02/27/16/10/tree-276014__340.jpg",
+        "https://cdn.pixabay.com/photo/2015/06/19/21/24/the-road-815297__340.jpg",
+        "https://cdn.pixabay.com/photo/2015/09/09/16/05/forest-931706__340.jpg"
+        ],
+        counter: 0
+    },
+
+    methods: {
+        clickNext: function() {
+            this.counter++;
+            if (this.counter == this.imgArray.length) {
+                this.counter = 0;
+            }
+        },
+
+        clickPrev: function() {
+            this.counter--;
+            if (this.counter < 0){
+                this.counter = this.imgArray.length - 1
+                console.log(this.counter);
+            }
+        }
+    }
+});
